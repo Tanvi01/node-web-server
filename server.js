ca884b678ccf83401056271180a4e6a84095b371
Tanvi01 <gtanvi24@gmail.com>
2018-07-05 14:54:03 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear()
+});
+app.use(express.static(__dirname+'/public'));
+app.use((req,res,next)=>{
+var now=new Date().toString();
+var log=`${now}: ${req.method} ${req.url}`;
+console.log(log);
+fs.appendFileSync('server.log',log + '\n');
+next();
+
+})
+
+app.get('/',(req,res)=>{
+	//res.send('<h1>Hello express!</h1>');
+	// res.send({
+	// 	name:'Tanvi',
+	// 	likes:[
+	// 	'biking','cities']
+	// });
+
+
+	res.render('home.hbs',{
+		welcomeMsg:'Hi!Welcome to the home page',
+		pageHeading:'Home'});
+});
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		pageHeading:'About Page'
+	});
+});
+app.get('/bad',(req,res)=>{
+	res.send({
+		errorMessage:'Unable to proceed madam'
+	});
+})
+app.listen(3000);
